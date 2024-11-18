82230015bba5ff18a9bf82269b2e0301dbafd06d
DANIEL JOANOL ARNDT <d.joanol@cleverpy.com>
2024-11-18 21:53:07 +0100

    Refactor sql_template

--- lib/core/domain/sql_template.dart
+++ lib/core/domain/sql_template.dart
@@ -11,50 +11,45 @@ class SqlTemplate {
       'INSERT INTO {table_name} ({field_names}) SELECT ({field_values}) WHERE NOT EXISTS (SELECT 1 FROM {table_name} WHERE {id_name} = {id_value})';
 
 
-  String insert({required String tableName,
+  String insert({
+      required String tableName,
       required Map<String, String> mapTextValuesByFieldName,
       Map<String, double>? mapNumberValuesByFieldName,
       int? rowNumber
   }) {
-    _generateJoints(
-        mapTextValuesByFieldName: mapTextValuesByFieldName,
-        mapNumberValuesByFieldName: mapNumberValuesByFieldName);
+    _generateJoints(mapTextValuesByFieldName, mapNumberValuesByFieldName);
 
     String statement = '';
     if (rowNumber != null) {
-      statement = _generate_comment(rowNumber);
+      statement = _generateComment(rowNumber);
     }
     
     String? idValue = mapTextValuesByFieldName[idFieldName];
-    return statement += _generate_insert_statement(tableName, idValue);
+    return statement += _generateInsertStatement(tableName, idValue);
   }
 
 
-  void _generateJoints({required Map<String, String> mapTextValuesByFieldName, Map<String, double>? mapNumberValuesByFieldName}) {
+  void _generateJoints(Map<String, String> mapTextValuesByFieldName, Map<String, double>? mapNumberValuesByFieldName) {
     bool hasNumbers = mapNumberValuesByFieldName != null && mapNumberValuesByFieldName.isNotEmpty;
     if (hasNumbers) {
       jointKeys = _generateJointVariables(
-          textValues: mapTextValuesByFieldName.keys.toList(),
-          numberValues:  mapNumberValuesByFieldName.keys.toList()
+          mapTextValuesByFieldName.keys.toList(),
+          mapNumberValuesByFieldName.keys.toList()
       );
       jointValues = _generateJointVariables(
-          textValues: mapTextValuesByFieldName.values.toList(),
-          numberValues: mapNumberValuesByFieldName.values.toList()
+          mapTextValuesByFieldName.values.toList(),
+          mapNumberValuesByFieldName.values.toList()
       );
     } else {
-      jointKeys = _generateJointVariables(
-          textValues: mapTextValuesByFieldName.keys.toList()
-      );
-      jointValues = _generateJointVariables(
-          textValues: mapTextValuesByFieldName.values.toList()
-      );
+      jointKeys = _generateJointVariables(mapTextValuesByFieldName.keys.toList(), null);
+      jointValues = _generateJointVariables(mapTextValuesByFieldName.values.toList(), null);
     }
   }
 
 
-  String _generateJointVariables({required List<String> textValues, List<dynamic>? numberValues}) {
+  String _generateJointVariables(List<String> textValues, List<dynamic>? numberValues) {
     var jointTextValues = textValues.join(',');
-    bool hasNumberValues = numberValues != null && !numberValues.isEmpty;
+    bool hasNumberValues = numberValues != null && numberValues.isNotEmpty;
     if (hasNumberValues) {
       var jointNumberValues = numberValues.join(',');
       return '$jointTextValues, $jointNumberValues';
@@ -64,15 +59,15 @@ class SqlTemplate {
   }
 
 
-  String _generate_comment(int rowNumber) {
+  String _generateComment(int rowNumber) {
     Map<String, String> values = {
       'row_number': rowNumber.toString()
     };
-    return _format(template: comment, values: values);
+    return _format(comment, values);
   }
 
 
-  String _generate_insert_statement(String tableName, String? idValue) {
+  String _generateInsertStatement(String tableName, String? idValue) {
 
     Map<String, dynamic> values = {
       'table_name': tableName,
@@ -82,11 +77,11 @@ class SqlTemplate {
       'id_value': idValue
     };
 
-    return _format(template: insertTemplate, values: values);
+    return _format(insertTemplate, values);
   }
-  
 
-  String _format({required String template, required Map<String, dynamic> values}) {
+
+  String _format(template, Map<String, dynamic> values) {
     return template.replaceAllMapped(RegExp(r'\{\w+\}'), (match) {
       String key = match.group(0)!.replaceAll(RegExp(r'[{}]'), '');
       return values[key] ?? match.group(0)!;
